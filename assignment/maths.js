21f29eae1fb92f10557ec1768cf0e31fe6b8f8ed
Ajmubby <ajuwonmubarak4@gmail.com>
2024-01-15 22:11:55 +0100

    feat: add assingment

---
+++ assignment/maths.js
@@ -0,0 +1,10 @@
+const num1 = 10
+const num2 = 20
+const num3 = 35
+
+console.log(`The addition of num2 and num3 is: ${num2 + num3}`)
+console.log(`The subtraction of num3 and num1 is: ${num3 - num1}`)
+console.log(`The division of num2 and num1 is: ${num2 / num1}`)
+console.log(`The multiplication of num2 and num1 is: ${num2 * num1}`)
+console.log(`The modulous of num3 and num1 is: ${num3 % num1}`)
+
---
+++ assignment/string.js
@@ -0,0 +1,14 @@
+let myName = 'Ajuwon Mubarak. '
+
+
+console.log('length of string: '+ myName.length);
+console.log('To UpperCase: ' + myName.toUpperCase());
+console.log('To LowerCase: ' + myName.toLowerCase());
+console.log('charAt: ' + myName.charAt(2).toUpperCase());
+console.log('The substring method: ' + myName.substring(0, 6));
+console.log('Replace "Ajuwon" with "Adetola": ' + myName.replace('Ajuwon', 'Adetola'));
+console.log('Index number of "w": ' + myName.indexOf('w'));
+const greetings = `you\'re welcome to Frontend ${myName}`
+console.log(greetings)
+console.log(myName > greetings)
+
