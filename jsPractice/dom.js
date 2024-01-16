d7481b0fca829b079356c76cf65e73969a5aea69
Ajmubby <ajuwonmubarak4@gmail.com>
2024-01-17 00:05:54 +0100

    feat: add work

---
+++ jsPractice/dom.js
@@ -0,0 +1,8 @@
+let title = document.querySelector('h2')
+
+title.textContent = 'JavaScript'
+
+let change = document.querySelector('h1')
+change.textContent = ('Welcome back Mubby')
+
+const para1 = document.querySelector('p')
