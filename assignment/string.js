d836e0326053a354f0a622a70b38c1ecbaad847e
Ajmubby <ajuwonmubarak4@gmail.com>
2024-01-16 23:01:27 +0100

    feat: add work

--- assignment/string.js
+++ assignment/string.js
@@ -6,7 +6,7 @@ console.log('To UpperCase: ' + myName.toUpperCase());
 console.log('To LowerCase: ' + myName.toLowerCase());
 console.log('charAt: ' + myName.charAt(2).toUpperCase());
 console.log('The substring method: ' + myName.substring(0, 6));
-console.log('Replace "Ajuwon" with "Adetola": ' + myName.replace('Ajuwon', 'Adetola'));
+console.log('Replace "Ajuwon" with "Adetola": ' + myName.replace('Aju', 'Ade'));
 console.log('Index number of "w": ' + myName.indexOf('w'));
 const greetings = `you\'re welcome to Frontend ${myName}`
 console.log(greetings)
